605a8cb50c1591191371fe7628a01d3c30241589
emilyirenelai <emilyirenelai@gmail.com>
2023-10-14 10:47:41 -0400

    update question generation

--- src/question-gen.py
+++ src/question-gen.py
@@ -13,7 +13,9 @@ def generate_text(prompt, temp=0):
   return response.generations[0].text
 
 context = "When it comes to designing software, effective design is very important. Connecting to databases can be an extremely expensive task, particularly because of how much time it typically takes. The inefficient way to use a database connection would be connecting and disconnecting multiple times every time the database is required. Because of this, the best practice is to connect to it once, perform all the necessary functions dealing with the database, and then disconnect [x]. Such a practice can be followed by using the singleton pattern in database connections. This involves setting up the database only once. Then, whichever methods require calling the database will leverage this connection [x]. This type of design is efficient in multiple ways. The primary business concern would be that it is fast, meaning that it spends less company time and is therefore less expensive. It is also beneficial for the environment, as it saves energy and processing power [x+1]. The reason for this is that excessive connections and disconnections to the database are avoided."
-prompt = f"""Write questions based on this lesson: {context}, giving one question for every main topic. Write an answer on the following line.:"""
+prompt = f"""Write questions based on this lesson: {context}, giving one question for every main topic. Write an answer on the following line.:
+
+"""
 
 # Example format:
 # Input Lesson: Engineers, as practitioners of engineering, are professionals who invent, design, analyze, build and test machines, complex systems, structures, gadgets and materials to fulfill functional objectives and requirements while considering the limitations imposed by practicality, regulation, safety and cost.[1][2] The word engineer (Latin ingeniator[3]) is derived from the Latin words ingeniare ("to contrive, devise") and ingenium ("cleverness").[4][5] The foundational qualifications of a licensed professional engineer typically include a four-year bachelor's degree in an engineering discipline, or in some jurisdictions, a master's degree in an engineering discipline plus four to six years of peer-reviewed professional practice (culminating in a project report or thesis) and passage of engineering board examinations.
@@ -33,8 +35,15 @@ prompt = f"""Write questions based on this lesson: {context}, giving one questio
 # Question 6: List three responsibilities of an engineer during the engineering design process.
 # Answer 6: Defining problems, conducting and narrowing research, analyzing criteria, finding and analyzing solutions, making decisions.
 
-response = generate_text (prompt, temp=0.5)
-print(response)
+question_response = generate_text (prompt, temp=0.5)
+with open("question.txt", "r+") as q_text_file: 
+  # save response as text file
+  q_text_file.write(question_response)
+  
+  file_contents = q_text_file.read()
+  print(file_contents)
+  # first_line=q_text_file.readline().strip()
+# print(question_response)
 
 # take this response and instead of printing it
 # first line (question) becomes output for viewer to see
