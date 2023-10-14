d37215591ce240f401dd44debeb54fe936ffe5e8
Natasha Naorem <natasha.naorem@gmail.com>
2023-10-14 15:33:25 -0400

    Base of the Prep Screen (upload/ ar)

--- tutorbo/src/components/prep.js
+++ tutorbo/src/components/prep.js
@@ -1,12 +1,27 @@
-import React from 'react';
-//import "./prep.css";
+import React, {useState} from 'react';
+import "./prep.css";
 import { Link } from 'wouter';
 
+
 function GetNotes() {
+
+    const [file, setFile] = useState(null);
+
+    const onFileChange = (event) => {
+    setFile(event.target.files[0]);
+    console.log(event.target.files[0]);
+  };
+
     return (
         <div>
-            <button>Upload Files</button>
-            <Link to="/ar"><button >Scan with AR</button></Link>
+            <div className='bordered-div'>
+                <h1>Upload a File to Learn with Bo</h1>
+                <input type="file" onChange={onFileChange} />
+            </div>
+            <div className='bordered-div'>
+                <h1>Scan with AR and Play with Bo</h1>
+                <Link to="/ar"><button >Scan with AR</button></Link>
+            </div>
         </div>
     );
 }
