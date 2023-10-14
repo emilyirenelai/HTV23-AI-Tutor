892db5edd49f5e41a35fa3c11fec5de533d28410
Châu Nguyễn <chaunguyen@doofenshmirtz.local>
2023-10-14 12:52:55 -0400

    added routes and api documentation

---
+++ server/routes/recognize.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+const vision = require('@google-cloud/vision');
+
+router.post('/image', async function(req, res, next) {
+    // creates a client
+    const client = new vision.ImageAnnotatorClient();
+    const fileName = '/Users/chaunguyen/workspace/HTV23-AI-Tutor/server/routes/';
+
+    fs.writeFile('decoded-image.png', req.body.file, {encoding: 'base64'}, function(err) {
+        console.log('File created');
+    });
+
+    // performs text detection on the local file
+    const [result] = await client.textDetection(fileName + 'decoded-image.png');
+    const detections = result.textAnnotations;
+    console.log('Text:');
+    detections.forEach(text => console.log(text));
+    res.send(detections[0].description);
+});
+
+router.get('/handwritten', async function(req, res, next) {
+    // creates a client
+    const client = new vision.ImageAnnotatorClient();
+    const fileName = '/Users/chaunguyen/workspace/HTV23-AI-Tutor/server/routes/handwritten.jpeg';
+
+    // read a local image as a text document
+    const [result] = await client.documentTextDetection(fileName);
+    const fullTextAnnotation = result.fullTextAnnotation;
+    console.log(`Full text: ${fullTextAnnotation.text}`);
+    fullTextAnnotation.pages.forEach(page => {
+        page.blocks.forEach(block => {
+            console.log(`Block confidence: ${block.confidence}`);
+            block.paragraphs.forEach(paragraph => {
+                console.log(`Paragraph confidence: ${paragraph.confidence}`);
+                paragraph.words.forEach(word => {
+                    const wordText = word.symbols.map(s => s.text).join('');
+                    console.log(`Word text: ${wordText}`);
+                    console.log(`Word confidence: ${word.confidence}`);
+                    word.symbols.forEach(symbol => {
+                    console.log(`Symbol text: ${symbol.text}`);
+                    console.log(`Symbol confidence: ${symbol.confidence}`);
+                    });
+                });
+            });
+        });
+    });
+});
+
+router.get('/pdf', async function(req, res, next) {
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    /**
+     * TODO(developer): Uncomment the following lines before running the sample.
+     */
+    // Bucket where the file resides
+    // const bucketName = 'my-bucket';
+    // Path to PDF file within bucket
+    // const fileName = 'path/to/document.pdf';
+    // The folder to store the results
+    // const outputPrefix = 'results'
+
+    const gcsSourceUri = `gs://${bucketName}/${fileName}`;
+    const gcsDestinationUri = `gs://${bucketName}/${outputPrefix}/`;
+
+    const inputConfig = {
+        // Supported mime_types are: 'application/pdf' and 'image/tiff'
+        mimeType: 'application/pdf',
+        gcsSource: {
+            uri: gcsSourceUri,
+        },
+    };
+    const outputConfig = {
+        gcsDestination: {
+            uri: gcsDestinationUri,
+        },
+    };
+    const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
+        const request = {
+        requests: [
+            {
+            inputConfig: inputConfig,
+            features: features,
+            outputConfig: outputConfig,
+            },
+        ],
+    };
+
+    const [operation] = await client.asyncBatchAnnotateFiles(request);
+    const [filesResponse] = await operation.promise();
+    const destinationUri =
+    filesResponse.responses[0].outputConfig.gcsDestination.uri;
+    console.log('Json saved to: ' + destinationUri);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -6,9 +6,12 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // routes
-app.use("/text-recognition", require("./routes/text-recognition"));
+app.use("/encode", require("./routes/encode"));
+app.use("/recognize", require("./routes/recognize"));
+app.use("/summarize", require("./routes/summarize"));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
