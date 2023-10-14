55216d44221e08790b7ce7e5da221c1403193f90
Châu Nguyễn <chaunguyen@doofenshmirtz.local>
2023-10-14 17:04:40 -0400

    added unique naming to route /upload and wrapper to route /recognize

--- server/routes/recognize.js
+++ server/routes/recognize.js
@@ -3,95 +3,61 @@ const router = express.Router();
 const fs = require('fs');
 
 const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+const bucketName = process.env.BUCKET_NAME;
 
-router.get('/image', async function(req, res, next) {
-    // creates a client
-    const client = new vision.ImageAnnotatorClient();
+router.post('/', async function(req, res) {
+    if (!req.body.blobName) {
+        return res.status(400).send({ message: "Please specify a file name!" });
+    }
+    const fileName = req.body.blobName;
 
-    const bucketName = process.env.BUCKET_NAME;
-    const fileName = 'image.png';
+    // Handle PDF file
+    if (fileName.slice(-3).toLowerCase() == 'pdf') {
+         // The folder to store the results
+        const outputPrefix = 'results'
 
-    // performs text detection on the gcs file
-    const [result] = await client.textDetection(`gs://${bucketName}/${fileName}`);
-    const detections = result.textAnnotations;
-    console.log('Text:');
-    detections.forEach(text => console.log(text));
-    res.send(detections[0].description);
-});
+        // Set up Google Cloud Storage URI for target and result
+        const gcsSourceUri = `gs://${bucketName}/${fileName}`;
+        const gcsDestinationUri = `gs://${bucketName}/${outputPrefix}/`;
 
-router.get('/handwritten', async function(req, res, next) {
-    // creates a client
-    const client = new vision.ImageAnnotatorClient();
-    const fileName = '/Users/chaunguyen/workspace/HTV23-AI-Tutor/server/routes/handwritten.jpeg';
-
-    // read a local image as a text document
-    const [result] = await client.documentTextDetection(fileName);
-    const fullTextAnnotation = result.fullTextAnnotation;
-    console.log(`Full text: ${fullTextAnnotation.text}`);
-    fullTextAnnotation.pages.forEach(page => {
-        page.blocks.forEach(block => {
-            console.log(`Block confidence: ${block.confidence}`);
-            block.paragraphs.forEach(paragraph => {
-                console.log(`Paragraph confidence: ${paragraph.confidence}`);
-                paragraph.words.forEach(word => {
-                    const wordText = word.symbols.map(s => s.text).join('');
-                    console.log(`Word text: ${wordText}`);
-                    console.log(`Word confidence: ${word.confidence}`);
-                    word.symbols.forEach(symbol => {
-                    console.log(`Symbol text: ${symbol.text}`);
-                    console.log(`Symbol confidence: ${symbol.confidence}`);
-                    });
-                });
-            });
-        });
-    });
-});
-
-router.get('/pdf', async function(req, res, next) {
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
-
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    // Bucket where the file resides
-    // const bucketName = 'my-bucket';
-    // Path to PDF file within bucket
-    // const fileName = 'path/to/document.pdf';
-    // The folder to store the results
-    // const outputPrefix = 'results'
-
-    const gcsSourceUri = `gs://${bucketName}/${fileName}`;
-    const gcsDestinationUri = `gs://${bucketName}/${outputPrefix}/`;
-
-    const inputConfig = {
-        // Supported mime_types are: 'application/pdf' and 'image/tiff'
-        mimeType: 'application/pdf',
-        gcsSource: {
-            uri: gcsSourceUri,
-        },
-    };
-    const outputConfig = {
-        gcsDestination: {
-            uri: gcsDestinationUri,
-        },
-    };
-    const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
-        const request = {
-        requests: [
-            {
-            inputConfig: inputConfig,
-            features: features,
-            outputConfig: outputConfig,
+        const inputConfig = {
+            mimeType: 'application/pdf',
+            gcsSource: {
+                uri: gcsSourceUri,
             },
-        ],
-    };
+        };
+        const outputConfig = {
+            gcsDestination: {
+                uri: gcsDestinationUri,
+            },
+        };
+        const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
+            const request = {
+            requests: [
+                {
+                inputConfig: inputConfig,
+                features: features,
+                outputConfig: outputConfig,
+                },
+            ],
+        };
 
-    const [operation] = await client.asyncBatchAnnotateFiles(request);
-    const [filesResponse] = await operation.promise();
-    const destinationUri =
-    filesResponse.responses[0].outputConfig.gcsDestination.uri;
-    console.log('Json saved to: ' + destinationUri);
-});
+        const [operation] = await client.asyncBatchAnnotateFiles(request);
+        const [filesResponse] = await operation.promise();
+        const destinationUri = filesResponse.responses[0].outputConfig.gcsDestination.uri;
+        console.log('Json saved to: ' + destinationUri);
+        res.end();
+    }
+    // Hanlde image file
+    else {
+        const [result] = await client.documentTextDetection(
+            `gs://${bucketName}/${fileName}`
+        );
+        const fullTextAnnotation = result.fullTextAnnotation;
+        console.log(fullTextAnnotation.text);
+        res.send(fullTextAnnotation.text);
+    }
+})
 
 module.exports = router;
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const fs = require('fs');
 const { Storage } = require("@google-cloud/storage");
 const Multer = require("multer");
+const { v4: uuidv4 } = require('uuid');
 
-// instantiate a storage client with credentials
+// Instantiate a storage client with credentials
 const storage = new Storage({
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   });
@@ -13,7 +14,7 @@ const storage = new Storage({
 const multer = Multer({
     storage: Multer.memoryStorage(),
     limits: {
-      fileSize: 25 * 1024 * 1024, // no larger than 5mb
+      fileSize: 25 * 1024 * 1024, // no larger than 5MB
     },
 });
 
@@ -23,7 +24,8 @@ router.post("/", multer.single("file"), function(req, res) {
             return res.status(400).send({ message: "Please upload a file!" });
         }
     
-        const blob = bucket.file(req.file.originalname);
+        const extension = req.file.originalname.split('.').pop();
+        const blob = bucket.file(uuidv4() + '.' + extension);
         const blobStream = blob.createWriteStream({
           resumable: false,
         });
@@ -33,21 +35,9 @@ router.post("/", multer.single("file"), function(req, res) {
         });
     
         blobStream.on("finish", async (data) => {
-            // create a url to access file
-            const publicURL = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-        
-            try {
-                await bucket.file(req.file.originalname).makePublic();
-            } catch {
-                return res.status(500).send({
-                message: `Uploaded the file successfully: ${req.file.originalname}, but public access is denied!`,
-                url: publicURL,
-                });
-            }
-        
             res.status(200).send({
                 message: "Uploaded the file successfully: " + req.file.originalname,
-                url: publicURL,
+                blobName: blob.name,
             });
         });
         blobStream.end(req.file.buffer);
